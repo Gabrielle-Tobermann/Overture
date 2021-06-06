7394b27c931bba5e05244f06f25f3d7f9eba3aee
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-06-06 18:33:58 -0500

    added authentication

--- src/App/index.js
+++ src/App/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import firebase from 'firebase';
 import Footer from '../components/Footer';
 import NavBar from '../components/Navbar';
 import Routes from '../helpers/routes';
 import './App.scss';
 
 function App() {
+  const [user, setUser] = useState(null);
+  // const [admin, setAdmin] = useState(null);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((authed) => {
+      if (authed) {
+        const userObj = {
+          fullName: authed.displayName,
+          profileImage: authed.photoURL,
+          uid: authed.uid,
+          user: authed.email.split('@')[0]
+        };
+        setUser(userObj);
+      } else if (user || user === null) {
+        setUser(false);
+      }
+    });
+  }, []);
+
   return (
     <div className='App'>
     <Router>
-      <NavBar/>
+      <NavBar
+      user={user}
+      />
       <Routes/>
     </Router>
     <Footer/>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import {
   Collapse,
   Navbar,
@@ -8,11 +9,32 @@ import {
   Button
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { signInUser, signOutUser } from '../helpers/auth';
 
-const NavBar = () => {
+const NavBar = ({ user }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
+
+  const authenticated = () => (
+    <>
+      <NavItem>
+      <Link className="nav-link" to='/'>Home</Link>
+    </NavItem>
+    <NavItem>
+      <Link className="nav-link" to='/instrument-inventory'>Instrument Inventory</Link>
+    </NavItem>
+    <NavItem>
+      <Link className="nav-link" to='/bow-inventory'>Bow Inventory</Link>
+    </NavItem>
+    <NavItem>
+      <Link className="nav-link" to='/orders'>Orders</Link>
+    </NavItem>
+    <NavItem>
+      <Link className="nav-link" to='/financial-reports'>Financial Reports</Link>
+    </NavItem>
+    </>
+  );
 
   return (
     <div>
@@ -20,27 +42,28 @@ const NavBar = () => {
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
-          <NavItem>
-              <Link className="nav-link" to='/'>Home</Link>
-            </NavItem>
-            <NavItem>
-              <Link className="nav-link" to='/instrument-inventory'>Instrument Inventory</Link>
-            </NavItem>
-            <NavItem>
-              <Link className="nav-link" to='/bow-inventory'>Bow Inventory</Link>
-            </NavItem>
-            <NavItem>
-              <Link className="nav-link" to='/orders'>Orders</Link>
-            </NavItem>
-            <NavItem>
-              <Link className="nav-link" to='/financial-reports'>Financial Reports</Link>
-            </NavItem>
-          </Nav>
-          <Button>Sign In</Button>
+         {user && authenticated()}
+            </Nav>
+          <div>
+          {
+            user !== null
+            && <NavItem>
+              {
+                user
+                  ? <Button onClick={signOutUser}>Sign Out</Button>
+                  : <Button onClick={signInUser}>Sign In</Button>
+              }
+              </NavItem>
+          }
+          </div>
         </Collapse>
       </Navbar>
     </div>
   );
+};
+
+NavBar.propTypes = {
+  user: PropTypes.any
 };
 
 export default NavBar;
