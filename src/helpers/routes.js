d5d93988fea6120f11e7432ab857d39197aab8b2
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-06-07 19:40:51 -0500

    private routes are working properly for admin and user

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -7,9 +7,11 @@ import FinancialReportsView from '../views/FinancialReportsView';
 import InstrumentView from '../views/InstrumentView';
 import OrderView from '../views/OrderView';
 
-const PrivateRoute = ({ component: Component, user, ...rest }) => {
-  const routeChecker = (remainder) => (user
-    ? (<Component {...remainder} user={user} />)
+const PrivateRoute = ({
+  component: Component, user, admin, ...rest
+}) => {
+  const routeChecker = (remainder) => (user || admin
+    ? (<Component {...remainder} user={user} admin={admin}/>)
     : (<Redirect to={{ pathname: '/', state: { from: remainder.location } }} />));
 
   return <Route {...rest} render={(props) => routeChecker(props)} />;
@@ -17,21 +19,22 @@ const PrivateRoute = ({ component: Component, user, ...rest }) => {
 
 PrivateRoute.propTypes = {
   component: PropTypes.func,
-  user: PropTypes.any
-};
-
-const AdminRoute = ({ component: Component, admin, ...rest }) => {
-  const routeChecker = (remainder) => (admin
-    ? (<Component {...remainder} admin={admin} />)
-    : (<Redirect to={{ pathname: '/', state: { from: remainder.location } }} />));
-
-  return <Route {...rest} render={(props) => routeChecker(props)} />;
-};
-
-AdminRoute.propTypes = {
-  component: PropTypes.func,
+  user: PropTypes.any,
   admin: PropTypes.any
 };
+
+// const AdminRoute = ({ component: Component, admin, ...rest }) => {
+//   const routeChecker = (remainder) => (admin
+//     ? (<Component {...remainder} admin={admin} />)
+//     : (<Redirect to={{ pathname: '/', state: { from: remainder.location } }} />));
+
+//   return <Route {...rest} render={(props) => routeChecker(props)} />;
+// };
+
+// AdminRoute.propTypes = {
+//   component: PropTypes.func,
+//   admin: PropTypes.any
+// };
 
 function Routes({ user, admin }) {
   return (
@@ -45,16 +48,19 @@ function Routes({ user, admin }) {
         <PrivateRoute exact path='/instrument-inventory'
         component={InstrumentView}
         user={user}
+        admin={admin}
         />
         <PrivateRoute exact path='/bow-inventory'
         component={BowView}
         user={user}
+        admin={admin}
         />
         <PrivateRoute exact path='/orders'
         component={OrderView}
         user={user}
+        admin={admin}
         />
-        <AdminRoute exact path='/financial-reports'
+        <PrivateRoute exact path='/financial-reports'
         component={FinancialReportsView}
         admin={admin}
         />
