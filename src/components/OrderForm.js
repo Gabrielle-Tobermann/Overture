1d7327c3a0ec7a08d3b114b68ef24814e85e53f6
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-06-10 13:12:06 -0500

    input field can be added or removed. Onchange, hook updates

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -6,37 +6,63 @@ import {
   Label,
   Input
 } from 'reactstrap';
+import { v4 as uuidv4 } from 'uuid';
 
 function OrderForm() {
-  const [itemInput, setItemInput] = useState([{
-    itemID: ''
+  const [itemInputs, setItemInputs] = useState([{
+    itemID: '', id: uuidv4()
   }]);
 
   const addNewField = () => {
-    
-  }
+    setItemInputs([...itemInputs, { id: uuidv4(), itemID: '' }]);
+  };
+
+  const removeField = (id) => {
+    const inputs = [...itemInputs];
+    inputs.splice(inputs.findIndex((element) => element.id === id));
+    setItemInputs(inputs);
+  };
+
+  const handleItemInputChange = (id, e) => {
+    const newInputs = itemInputs.map((element) => {
+      if (id === element.id) {
+        const el = element;
+        el[e.target.name] = e.target.value;
+      }
+      console.warn(e.target.value);
+      return element;
+    });
+    setItemInputs(newInputs);
+    console.warn(newInputs);
+  };
   return (
     <div>
        <Form>
        <FormGroup>
-        <Label for="fullName">Customer`&apos;s Full Name:</Label>
+        <Label for="fullName">Customer&apos;s Full Name:</Label>
         <Input type="text" name="fullName" id="fullName" placeholder="Enter name" />
       </FormGroup>
       <FormGroup>
-        <Label for="email">Customer`&apos;s Email:</Label>
+        <Label for="email">Customer&apos;s Email:</Label>
         <Input type="email" name="email" id="email" placeholder="Enter email" />
       </FormGroup>
       <div>
         {
-          itemInput.map((item, i) => (
-            <>
-              <FormGroup key={i}>
+          itemInputs.map((item) => (
+            <div key={item.id}>
+              <FormGroup>
               <Label for="item">Item</Label>
-              <Input type="text" name="itemID" id={`item${i}`} placeholder="Enter item ID" value={item.itemID}/>
+              <Input type="text"
+              name="itemID"
+              id={item.id}
+              placeholder="Enter item ID"
+              value={item.itemID}
+              onChange={(e) => handleItemInputChange(item.id, e)}
+              />
               </FormGroup>
               <Button onClick={addNewField}>+</Button>
               <Button onClick={removeField}>-</Button>
-            </>
+            </div>
           ))
         }
       </div>
