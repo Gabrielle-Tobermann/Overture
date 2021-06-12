f78b5ea63692230f92b12be229869b90230441ef
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-06-12 11:59:55 -0500

    working on netlify stuff

--- src/functions/get-products.js
+++ src/functions/get-products.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const products = axios.get('https://overture-7097e-default-rtdb.firebaseio.com/items')
+const products = axios('https://overture-7097e-default-rtdb.firebaseio.com/items')
   .then((response) => response.json())
   .catch((error) => console.warn(error));
 
@@ -8,5 +8,5 @@ console.warn(products);
 
 exports.handler = async () => ({
   statusCode: 200,
-  body: JSON.stringify(products)
+  body: JSON.stringify({ message: 'Hello' })
 });
