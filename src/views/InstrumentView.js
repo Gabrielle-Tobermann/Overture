f13164c4364091af661b96f2c840a18b108f8039
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-06-15 19:00:55 -0500

    can search instruments through search bar

--- src/views/InstrumentView.js
+++ src/views/InstrumentView.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'reactstrap';
+import { Button, Input } from 'reactstrap';
 import ItemCard from '../components/ItemCard';
 import ItemForm from '../components/ItemForm';
 import {
@@ -9,19 +9,44 @@ import {
 
 function InstrumentView({ items, setItems }) {
   const [adding, setAdding] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleButtonClick = () => {
     setAdding(true);
   };
 
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div>
       <ViewTitle>Instrument Inventory</ViewTitle>
+      <Input placeHolder='Search' value={searchTerm} onChange={handleSearchChange}></Input>
       <Button color="dark" style={{ fontSize: '20px' }} onClick={handleButtonClick}>+</Button>
       {adding && <ItemForm
       setItems={setItems}
       items={items}
       />}
+      { searchTerm === ''
+        ? ''
+        : items.filter((item) => item.itemType === 'instrument' && item.itemID.toLowerCase().includes(searchTerm.toLowerCase())).map((item) => (
+          <ItemCard
+            key={item.firebaseKey}
+            itemID={item.itemID}
+            image={item.image}
+            type={item.type}
+            size={item.size}
+            rental={item.rental}
+            price={item.price}
+            available={item.available}
+            firebaseKey={item.firebaseKey}
+            setItems={setItems}
+            items={items}
+            itemType={item.itemType}
+            />
+        ))
+      })
       <WholeCardContainer>
         <CardContainer>
           <InstrumentTitle>Violins</InstrumentTitle>
